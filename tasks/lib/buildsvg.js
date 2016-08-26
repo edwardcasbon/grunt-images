4271498924bf5b8bcf2435335bf33c449152c636
Edward Casbon <edward@edwardcasbon.co.uk>
2016-08-26 16:05:11 +0100

    Updated buildsvg script to use Grunt filesystem api.

--- tasks/lib/buildsvg.js
+++ tasks/lib/buildsvg.js
@@ -1,41 +1,27 @@
 'use strict';
 
-var fs = require('fs');
+var grunt = require('grunt');
 var DOMParser = require('xmldom').DOMParser;
 var XMLSerializer = require('xmldom').XMLSerializer;
-var mkdirp = require('mkdirp');
-var path = require('path');
 
 var buildSvg = {
     build: function(args, cb) {
-        fs.readFile(args[0], 'utf8', function(err, data){
-            if(err) {
-                console.error(err);
-            }
+        var data = grunt.file.read(args[0]);
+        if(!data) {
+            grunt.log.error('Error reading SVG file');
+        }
 
-            var parser = new DOMParser();
-            var svg = parser.parseFromString(data, "image/svg+xml");
+        var parser = new DOMParser();
+        var svg = parser.parseFromString(data, "image/svg+xml");
 
-            svg.documentElement.setAttribute('width', args[2] + 'px');
-            svg.documentElement.setAttribute('height', args[2] + 'px');
+        svg.documentElement.setAttribute('width', args[2] + 'px');
+        svg.documentElement.setAttribute('height', args[2] + 'px');
 
-            var XMLSerializerInstance = new XMLSerializer();
-            var svgAsString = XMLSerializerInstance.serializeToString(svg);
+        var XMLSerializerInstance = new XMLSerializer();
+        var svgAsString = XMLSerializerInstance.serializeToString(svg);
 
-            // Create output directory if it doesn't exist already.
-            mkdirp(path.dirname(args[1]), function(err) {
-                if(err) {
-                    console.error(err);
-                }
-
-                // Save the svg file.
-                fs.writeFile(args[1], svgAsString, function(err) {
-                    if(err) {
-                        console.error(err);
-                    }
-                });
-            });
-        });
+        // Save the svg file.
+        grunt.file.write(args[1], svgAsString);
     }
 };
 
